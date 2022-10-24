1d68ab229eec27281c0a3115dd0370b4ca221869
pablomunozSandoval <k_pablo17@hotmail.com>
2022-10-24 20:25:05 -0300

    Teclado Completado

--- Index.py
+++ Index.py
@@ -4,26 +4,59 @@ raiz = Tk()
 miFrame=Frame(raiz)
 miFrame.pack()
 
-
 #-------------Pantalla
-pantalla=Entry(miFrame)
-pantalla.grid(row=1,column=1,padx=10,pady=10)
+numPantalla=StringVar()
+
+pantalla=Entry(miFrame,textvariable=numPantalla)
+pantalla.grid(row=1,column=1,padx=10,pady=10,columnspan=4)
 pantalla.config(background="gray",fg="black",justify="right")
-pantalla.pack(fill="x",expand=True)
+#------------Presionar
 
 
-#------------Teclado Numérico
-btn1=Button(miFrame,text="1",width=3)
-btn1=grid(row=0)
-btn2=Button(miFrame,text="1",width=3)
-btn3=Button(miFrame,text="1",width=3)
-btn4=Button(miFrame,text="1",width=3)
-btn5=Button(miFrame,text="1",width=3)
-btn6=Button(miFrame,text="1",width=3)
-btn7=Button(miFrame,text="1",width=3)
-btn8=Button(miFrame,text="1",width=3)
-btn9=Button(miFrame,text="1",width=3)
-btn0=Button(miFrame,text="1",width=3)
+def numPulsado(num):
+	mirarPantalla=numPantalla.get()
+	numPantalla.set(mirarPantalla+num)
+
+def numSuma():
+	mirarPantalla=numPantalla.get()
+
+
+#------------Fila1
+btn1=Button(miFrame,text="1",width=3,command=lambda:numPulsado("1"))
+btn1.grid(row=2,column=1)
+btn2=Button(miFrame,text="2",width=3,command=lambda:numPulsado("2"))
+btn2.grid(row=2,column=2)
+btn3=Button(miFrame,text="3",width=3,command=lambda:numPulsado("3"))
+btn3.grid(row=2,column=3)
+btnSuma=Button(miFrame,text="+",width=3,command=lambda:numPulsado("+"))
+btnSuma.grid(row=2,column=4)
+#------------Fila2
+btn4=Button(miFrame,text="4",width=3,command=lambda:numPulsado("4"))
+btn4.grid(row=3,column=1)
+btn5=Button(miFrame,text="5",width=3,command=lambda:numPulsado("5"))
+btn5.grid(row=3,column=2)
+btn6=Button(miFrame,text="6",width=3,command=lambda:numPulsado("6"))
+btn6.grid(row=3,column=3)
+btnResta=Button(miFrame,text="-",width=3,command=lambda:numPulsado("-"))
+btnResta.grid(row=3,column=4)
+#------------Fila3
+btn7=Button(miFrame,text="7",width=3,command=lambda:numPulsado("7"))
+btn7.grid(row=4,column=1)
+btn8=Button(miFrame,text="8",width=3,command=lambda:numPulsado("8"))
+btn8.grid(row=4,column=2)
+btn9=Button(miFrame,text="9",width=3,command=lambda:numPulsado("9"))
+btn9.grid(row=4,column=3)
+btnDiv=Button(miFrame,text="/",width=3,command=lambda:numPulsado("/"))
+btnDiv.grid(row=4,column=4)
+#------------Fila4
+btnIgual=Button(miFrame,text="=",width=3,command=lambda:numPulsado("="))
+btnIgual.grid(row=5,column=1)
+btn0=Button(miFrame,text="0",width=3,command=lambda:numPulsado("0"))
+btn0.grid(row=5,column=2)
+btnComa=Button(miFrame,text=",",width=3,command=lambda:numPulsado(","))
+btnComa.grid(row=5,column=3)
+btnMult=Button(miFrame,text="x",width=3,command=lambda:numPulsado("*"))
+btnMult.grid(row=5,column=4)
 
 
 raiz.mainloop()
